2ab3bba01a2bb055c4f494c271fad541a577ae74
SohitRaj <101971842+SohitRaj@users.noreply.github.com>
2024-10-16 10:05:08 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,9 @@
+"Use strict"; // treat all javascript code as newer version
+// alert(3+3) // we are using nodejs not browser
+console.log(3
+    +
+     3) // code readbility should be high
+     console.log("sohit")
+     let name= "sohit"
+     let age= 17
+     let isloggedIn= false
